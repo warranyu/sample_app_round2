250e32b6e5cacc1cec5a1bcd5701c8430d0069da
Warranyu Walton <warranyu@gmail.com>
2012-12-12 23:37:01 -0500

    Initial commit for sample_app with Ruby 1.9.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '60e55251b6c8a6afe8db82a9af381b2c87fa54a39358828d6b048a04210c36dc1dbbfe0b1252e5436eec3c7db4920e1b14a7b4e096f5bb9dac82b5b4182e92c9'
